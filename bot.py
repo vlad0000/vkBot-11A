434858305612942e0d3f5630e20ec37db6e7b9c5
vlad0000 <52610344+vlad0000@users.noreply.github.com>
2020-09-21 22:33:37 +0300

    v1.1.0
    отвечает в беседах

--- bot.py
+++ bot.py
@@ -1,24 +1,33 @@
 import random
+import math
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 from toks import main_token
 
+def random_game(sender, message=''):
+  
+  start = 'Я загадаю число от 1 до '
+  write_message(sender, message)
+
 def write_message(sender, message):
-  vk.method('messages.send', {'user_id': sender, 'message': message, 'random_id': 0})
+  vk.method('messages.send', {'chat_id': sender, 'message': message, 'random_id': 0})
   print('id: ', sender, 'message: ', message) 
 
 vk = vk_api.VkApi(token=main_token)
-longpoll = VkLongPoll(vk)
+longpoll = VkBotLongPoll(vk, group_id=198845838)
+
 listHi = ['здравствуйте', 'привет', 'салам', 'добрый день', 'добро пожаловать', 'хай', 'салам алейкум', 'приветик', 'прив']
 
-
 for event in longpoll.listen():
-  if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-    reseived_message = event.text
-    sender = event.user_id
+  if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != '':
+    reseived_message = event.message.get('text')
+    sender = event.chat_id
+    print('id: ', sender, 'msg: ', reseived_message)
     if reseived_message.lower() in listHi:
       write_message(sender, random.choice(listHi).title())
-    else:
-      write_message(sender, 'Я еще не научен понимать такие команды &#128553;&#128553;&#128553;')
+    elif reseived_message.lower() == 'rd':
+      random_game(sender, message)
+    
+    
 
