3c096de33b0c1c66b6ea03267f809f3b8fbde859
vlad0000 <52610344+vlad0000@users.noreply.github.com>
2020-09-22 19:52:05 +0300

    v1.2.0
    Отвечает в беседах
    
    Обращается по имени
    
    Может выбрать рандомного пользователя в беседе по команде ['кому повезло?']

--- bot.py
+++ bot.py
@@ -3,31 +3,52 @@ import math
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
-from toks import main_token
+from toks import main_token, main_group_id
 
-def random_game(sender, message=''):
+def random_game(peer_id,reseived_message, message):
+  lvl = [32, 64, 128, 256]
+  num_max = random.choice(lvl)
+  # ans = random.randint(1, num_max)
+  attempt = int(math.log2(num_max))
+  # write_message(peer_id, ('Я загадал абсолютно рандомное число от 1 до ' + str(num_max) + ' \n- твоя задача отгадать какое это число за  ' + str(attempt) + ' попыток' + '\n Я буду лишь подсказывать больше или меньше твое число'))
   
-  start = 'Я загадаю число от 1 до '
-  write_message(sender, message)
+def write_message(peer_id,sender_first_name ,message ):
+  vk.method('messages.send', {'peer_id': peer_id, 'message': sender_first_name + ', ' + str(message), 'random_id': 0})
+  print('\nbot: ', message) 
 
-def write_message(sender, message):
-  vk.method('messages.send', {'chat_id': sender, 'message': message, 'random_id': 0})
-  print('id: ', sender, 'message: ', message) 
+def first_name():
+  fn = ''
+  for user in members['profiles']:
+    if user['id'] == from_id:
+      fn = user['first_name']
+      break
+  return fn
 
-vk = vk_api.VkApi(token=main_token)
-longpoll = VkBotLongPoll(vk, group_id=198845838)
+def random_user_cool(peer_id):
+   user = random.choice(members['profiles'])
+   random_user = user['first_name'] + ' ' + user['last_name']
+   message = 'счасливчиком является - '+ random_user
+   write_message(peer_id, sender_first_name, message)
+   
+vk = vk_api.VkApi(token = main_token)
+longpoll = VkBotLongPoll(vk, group_id = main_group_id)
+vk._auth_token()
 
-listHi = ['здравствуйте', 'привет', 'салам', 'добрый день', 'добро пожаловать', 'хай', 'салам алейкум', 'приветик', 'прив']
+listHi = ['здравствуйте', 'ку','привет' ,'салам' ,'добрый день' ,'добро пожаловать', 'хай', 'салам алейкум', 'приветик', 'прив']
 
 for event in longpoll.listen():
   if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != '':
     reseived_message = event.message.get('text')
-    sender = event.chat_id
-    print('id: ', sender, 'msg: ', reseived_message)
-    if reseived_message.lower() in listHi:
-      write_message(sender, random.choice(listHi).title())
-    elif reseived_message.lower() == 'rd':
-      random_game(sender, message)
-    
-    
 
+    from_id = event.message.get('from_id')
+    peer_id = event.message.get('peer_id')
+    members = vk.method('messages.getConversationMembers', {'peer_id': peer_id, 'fields': 'first_name', 'group_id': main_group_id})
+    members_count = members['count']
+    sender_first_name = first_name()
+    print('\n\npeer_id:', peer_id,'\nfrom_id:', from_id , ' : ', reseived_message)
+
+    
+    if reseived_message.lower() in listHi:
+      write_message(peer_id, sender_first_name,random.choice(listHi).title())
+    elif reseived_message.lower() == 'user_cool':
+      random_user_cool(peer_id)
--- toks.py
+++ toks.py
@@ -1,1 +1,105 @@
-main_token = '8e9588eb588c4c8175f3451c41b7e8e5a559eee742d680ab816c4aee6b1cbebfc9b969a91562ab0a3d98e'
+main_token = '2ebe22436fbba2ed77883e3239538ff7d6433f678369e3e4034d323269b57d918f3569eef65db2b7b6fe8'
+main_group_id = 198845838
+
+# {
+#   'items': [
+#     {
+#       'member_id': 282390438, 
+#       'invited_by': 282390438, 
+#       'join_date': 1600689210, 
+#       'is_admin': True, 
+#       'is_owner': True
+#     }, 
+#       {'member_id': 252525467, 
+#       'can_kick': True, 
+#       'invited_by': 282390438, 
+#       'join_date': 1600689210
+#     }, 
+#     {
+#       'member_id': -198845838, 
+#       'invited_by': 282390438, 
+#       'join_date': 1600710708, 
+#       'is_admin': True
+#     }, 
+#     {
+#       'member_id': 243461794, 
+#       'can_kick': True, 
+#       'invited_by': 282390438, 
+#       'join_date': 1600718568
+#     {
+#     'member_id': 458565781, 
+#       'can_kick': True, 
+#       'invited_by': 282390438, 
+#       'join_date': 1600724920
+#     }
+#   ], 
+#   'count': 5, 
+#   'profiles': [
+#     {
+#     'id': 243461794, 
+#     'first_name': 'Алексей', 
+#     'last_name': 'Шаврин', 
+#     'is_closed': True, 
+#     'can_access_closed': True
+#     }
+#     , {
+#       'id': 252525467, 
+#       'first_name': 'Вячеслав', 
+#       'last_name': 'Украинов', 
+#       'is_closed': False, 
+#       'can_access_closed': True}, 
+#     {
+#       'id': 282390438, 
+#       'first_name': 'Влад', 
+#       'last_name': 'Корнеев', 
+#       'is_closed': False, 
+#       'can_access_closed': True
+#     }, 
+#     {
+#       'id': 458565781, 
+#       'first_name': 'Ivan', 
+#       'last_name': 'Privoza', 
+#       'is_closed': False, 
+#       'can_access_closed': True
+#     }
+#   ], 
+#   'groups': [
+#     {
+#       'id': 198845838, 
+#       'name': '11A БОТ', 
+#       'screen_name': 'bot_11a', 
+#       'is_closed': 0, 
+#       'type': 'page', 
+#       'photo_50': 'https://vk.com/images/community_50.png?ava=1', 
+#       'photo_100': 'https://vk.com/images/community_100.png?ava=1', 
+#       'photo_200': 'https://vk.com/images/community_200.png?ava=1'
+#     }
+#   ]
+# }
+
+# <<class 'vk_api.bot_longpoll.VkBotMessageEvent'>({  
+#   'type': 'message_new',
+#   'object': {
+#     'message': {
+#       'date': 1600776151,
+#       'from_id': 282390438,
+#       'id': 0, 'out': 0,
+#       'peer_id': 2000000002,
+#       'text': '1',
+#       'conversation_message_id': 712,
+#       'fwd_messages': [],
+#       'important': False, 
+#       'random_id': 0, 
+#       'attachments': [], 
+#       'is_hidden': False}, 
+#       'client_info': {
+#           'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link'], 
+#           'keyboard': True, 
+#           'inline_keyboard': True, 
+#           'carousel': False, 
+#           'lang_id': 0
+#       }
+#   },
+#   'group_id': 198845838,
+#   'event_id': 'e03ba0ce8bdf1412c369faa957447e8c35059c9e'
+#  })>
